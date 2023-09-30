872affd1a532fb5a035544f1bbc83f17138d2a78
Matheus Páscui <121834658+matheuspascui@users.noreply.github.com>
2023-09-30 18:01:54 -0300

    updated version of "restaurant_meal_finder.py"

--- restaurant_meal_finder.py
+++ restaurant_meal_finder.py
@@ -13,16 +13,12 @@ noite - jantar
 '''
 import random
 # Função MAIN para conter o programa principal
-# Funções como Mostrar_refeicoes_cafe, Mostrar_refeicoes_almoco, chamadas baseadas em IF hora_refeicao da match
-# Carregar as listas built-in antes do programa começar (antes de chamar a MAIN)
+
 
 comidas_cafe_manha = ["Pão na chapa", "Café com leite", "Pão com requeijão", "Pão de queijo", "Frutas", "Iogurte natural", "Granola", "Sucrilhos"]
-comidas_almoco = ["Carnes Bovinas", "Carnes Suínas", "Frango", "Peixe", "Refrigerante", "Café expresso", "Salada", "Macarrão", "Prato feito"]
-comidas_jantar = ["Sanduíche", "Salada", "Prato feito", "Salgados", "Pão com frios", "Sopa"]
+comidas_almoco = ["Carnes Bovinas", "Carnes Suínas", "Frango", "Peixe", "Refrigerante", "Café expresso", "Salada", "Macarrão", "Prato feito", "Strogonoff", "Pastel e Caldo de cana"]
+comidas_jantar = ["Sanduíche", "Salada", "Prato feito", "Salgados", "Pão com frios", "Sopa", "Salgados fritos"]
 
-
-print("\nBem-vindo ao FoodFinder, seu ajudante para te dar sugestões de refeições para o seu dia!")
-hora_refeicao = input("Por favor, digite o período da refeição (manhã, tarde, noite): ")
 
 def opcoes_cafe_manha():
     print("Opções de café da manhã:\n")
@@ -49,49 +45,60 @@ def gravar_cafe():
     escolha_cafe = input(
         """\nVocê deseja escolher sua opção de café da manhã ou prefere que nosso assistente lhe dê uma sugestão?
 1 - Desejo escolher
-2 - Quero uma sugestão de café\t"""
+2 - Quero uma sugestão de café """
     )
     if escolha_cafe == "1":
-        logs_escolhas_cafe = []
-        escolha = input("Ok! Por favor, digite sua opção de café da mahã:\t")
-        logs_escolhas_cafe.append(escolha)
-        return logs_escolhas_cafe
+        escolha = input("Ok! Por favor, digite sua opção de café da mahã: ")
+        return escolha
     elif escolha_cafe == "2":
-        logs_escolhas_cafe = []
         escolha = random.choice(comidas_cafe_manha)
         print(f"Ok! Nossa sugestão de refeição para seu café da manhã é: {escolha}")
-        logs_escolhas_cafe.append(escolha)
-        return logs_escolhas_cafe
+        return escolha
     
 def gravar_almoco():
     escolha_almoco = input(
         """\nVocê deseja escolher sua opção de almoço ou deseja uma sugestão de nosso assitente?
 1 - Desejo escolher
-2 - Quero uma sugestão de almoço\t"""
+2 - Quero uma sugestão de almoço """
     )
     if escolha_almoco == "1":
-        logs_escolhas_almoco = []
-        escolha = input("Ok! Por favor, digite sua opção de almoço:\t")
-        logs_escolhas_almoco.append(escolha)
-        return logs_escolhas_almoco
-    elif logs_escolhas_almoco == "2":
-        logs_escolhas_almoco = []
+        escolha = input("Ok! Por favor, digite sua opção de almoço: ")
+        return escolha
+    elif escolha_almoco == "2":
         escolha = random.choice(comidas_almoco)
         print(f"Ok! Nossa sugestão de almoço para você hoje é: {escolha}")
-        logs_escolhas_almoco.append(escolha)
-        return logs_escolhas_almoco
+        return escolha
 
+def gravar_jantar():
+    escolha_jantar = input(
+        """\nVocê deseja escolher sua opção de almoço ou deseja uma sugestão de nosso assitente?
+1 - Desejo escolher
+2 - Quero uma sugestão de jantar """
+    )
+    if escolha_jantar == "1":
+        escolha = input("Ok! Por favor, digite sua opção de jantar: ")
+        return escolha
+    elif escolha_jantar == "2":
+        escolha = random.choice(comidas_almoco)
+        print(f"Ok! Nossa sugestão de jantar para você hoje é: {escolha}")
+        return escolha
 
-if hora_refeicao == "manhã" or hora_refeicao == "manha":
-    opcoes_cafe_manha()
-    cafe = gravar_cafe()
-    print(f"\nExcelente! Sua opção de hoje para o café da manhã foi: {cafe}")
-elif hora_refeicao == "tarde":
-    opcoes_almoco()
-    almoco = gravar_almoco()
-    print(f"\nExcelente! Sua opção de almoço apra hoje foi: {almoco}")
-elif hora_refeicao == "Noite" or hora_refeicao == "noite":
-    opcoes_jantar()
-else:
-    print("Opção Inválida")
+def main():
+    print("\nBem-vindo ao FoodFinder, seu ajudante para te dar sugestões de refeições para o seu dia!")
+    hora_refeicao = input("Por favor, digite o período da refeição (manhã, tarde, noite): ")
+    if hora_refeicao == "manhã" or hora_refeicao == "manha":
+        opcoes_cafe_manha()
+        cafe = gravar_cafe()
+        print(f"\nFinalizamos seu atendimento.\nSua opção para o dia de hoje foi: {cafe}\n")
+    elif hora_refeicao == "tarde":
+        opcoes_almoco()
+        almoco = gravar_almoco()
+        print(f"\nFinalizamos seu atendimento.\nSua opção para o dia de hoje foi: {almoco}\n")
+    elif hora_refeicao == "Noite" or hora_refeicao == "noite":
+        opcoes_jantar()
+        jantar = gravar_jantar()
+        print(f"\nFinalizamos seu atendimento.\nSua opção para o dia de hoje foi: {jantar}\n")
+    else:
+        print("Opção Inválida")
 
+main()
